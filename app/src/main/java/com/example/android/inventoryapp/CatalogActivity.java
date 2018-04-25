ab59d24625d427f22834dcc254bebdacea2dd85d
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-04-25 19:00:43 +0100

    editor activity

--- app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/CatalogActivity.java
@@ -1,14 +1,12 @@
 package com.example.android.inventoryapp;
 
 import android.content.ContentValues;
-import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.TextView;
 
 import com.example.android.inventoryapp.data.InventoryContract;
--- app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -1,4 +1,119 @@
 package com.example.android.inventoryapp;
 
-public class EditorActivity {
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.android.inventoryapp.data.InventoryContract;
+import com.example.android.inventoryapp.data.InventoryDbHelper;
+
+public class EditorActivity extends AppCompatActivity {
+
+    private EditText mNameEditText;
+
+    private EditText mPriceEditText;
+
+    private EditText mQuantityEditText;
+
+    private EditText mSupplierNameEditText;
+
+    private EditText mSupplierPhoneEditText;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        mNameEditText = (EditText) findViewById(R.id.edit_product_name);
+        mPriceEditText = (EditText) findViewById(R.id.edit_product_price);
+        mQuantityEditText = (EditText) findViewById(R.id.edit_product_quantity);
+        mSupplierNameEditText = (EditText) findViewById(R.id.edit_product_supplier_name);
+        mSupplierPhoneEditText = (EditText) findViewById(R.id.edit_product_supplier_phone);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Save pet to database
+                insertProduct();
+                // Exit activity
+                finish();
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (CatalogActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //methods needed
+    //Get user input from editor and save new pet into database.
+
+    private void insertProduct() {
+        //read inputted fields
+        //use trim to eliminate white spaces
+        String nameEditText = mNameEditText.getText().toString().trim();
+        String priceEditText = mPriceEditText.getText().toString().trim();
+        String quantityEditText = mQuantityEditText.getText().toString().trim();
+        String supplierNameEditText = mSupplierNameEditText.getText().toString().trim();
+        String supplierPhoneEditText = mSupplierPhoneEditText.getText().toString().trim();
+        int price = Integer.parseInt(priceEditText);
+        int quantity = Integer.parseInt(quantityEditText);
+
+        //create db helper
+        InventoryDbHelper mDbHelper = new InventoryDbHelper(this);
+
+        //get db in write mode
+        SQLiteDatabase db = mDbHelper.getWritableDatabase();
+
+        // Create a ContentValues object
+        ContentValues values = new ContentValues();
+        values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_NAME, nameEditText);
+        values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_PRICE, price);
+        values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, quantity);
+        values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_SUPPLIER_NAME, supplierNameEditText);
+        values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_SUPPLIER_PHONE, supplierPhoneEditText);
+
+        // Insert a new row for pet in the database,
+        long newRowId = db.insert(InventoryContract.ProductEntry.TABLE_NAME, null, values);
+
+        // Show a toast message depending on whether or not the insertion was successful
+        //fail
+        if (newRowId == -1) {
+            // If the row ID is -1, then there was an error with insertion.
+            Toast.makeText(this, getString(R.string.saving_error), Toast.LENGTH_SHORT).show();
+        }
+        //success
+        else {
+            // Otherwise, the insertion was successful and we can display a toast with the row ID.
+            Toast.makeText(this, getString(R.string.saving_success) + newRowId, Toast.LENGTH_SHORT).show();
+        }
+    }
 }
