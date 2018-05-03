e09a7e2bef49556a5d912ad0361a2c2a5a8b0883
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-03 18:57:01 +0100

    onCreate method implemented

--- app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -1,13 +1,18 @@
 package com.example.android.inventoryapp;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -18,8 +23,9 @@ import android.widget.Toast;
 import com.example.android.inventoryapp.data.InventoryContract;
 import com.example.android.inventoryapp.data.InventoryDbHelper;
 
-public class EditorActivity extends AppCompatActivity {
+public class EditorActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
+    //global variables needed
     private EditText mNameEditText;
 
     private EditText mPriceEditText;
@@ -30,11 +36,54 @@ public class EditorActivity extends AppCompatActivity {
 
     private EditText mSupplierPhoneEditText;
 
+    //Identifier for the product data loader
+    private static final int EXISTING_PRODUCT_LOADER = 0;
+
+    // Content URI for the existing product (null if it's a new product)
+    private Uri mCurrentProductUri;
+
+    // Boolean flag that keeps track of whether the product has been edited (true) or not (false)
+    private boolean mProductHasChanged = false;
+
+    // OnTouchListener that listens for any user touches on a View, implying that they are modifying
+    // the view, and we change the mProductHasChanged boolean to true.
+    // check if changes are made
+    // check if changes are made
+    private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            mProductHasChanged = true;
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
+
+        // Examine the intent that was used to launch this activity,
+        // in order to figure out if we're creating a new product or editing an existing one.
+        Intent intent = getIntent();
+        mCurrentProductUri = intent.getData();
+
+        // If the intent DOES NOT contain a product content URI, then we know that we are
+        // creating a new product.
+        if (mCurrentProductUri == null) {
+            // This is a new product, so change the app bar to say "Add a Product"
+            setTitle(getString(R.string.editor_activity_title_new_product));
+
+            // Invalidate the options menu, so the "Delete" menu option can be hidden.
+            // (It doesn't make sense to delete a product that hasn't been created yet.)
+            invalidateOptionsMenu();
+        } else {
+            // Otherwise this is an existing product, so change app bar to say "Edit Product"
+            setTitle(getString(R.string.editor_activity_title_edit_product));
+
+            // Initialize a loader to read the product data from the database
+            // and display the current values in the editor
+            getLoaderManager().initLoader(EXISTING_PRODUCT_LOADER, null, this);
+        }
 
         // Find all relevant views that we will need to read user input from
         mNameEditText = (EditText) findViewById(R.id.edit_product_name);
@@ -42,6 +91,15 @@ public class EditorActivity extends AppCompatActivity {
         mQuantityEditText = (EditText) findViewById(R.id.edit_product_quantity);
         mSupplierNameEditText = (EditText) findViewById(R.id.edit_product_supplier_name);
         mSupplierPhoneEditText = (EditText) findViewById(R.id.edit_product_supplier_phone);
+
+        // Setup OnTouchListeners on all the input fields, so we can determine if the user
+        // has touched or modified them. This will let us know if there are unsaved changes
+        // or not, if the user tries to leave the editor without saving.
+        mNameEditText.setOnTouchListener(mTouchListener);
+        mPriceEditText.setOnTouchListener(mTouchListener);
+        mQuantityEditText.setOnTouchListener(mTouchListener);
+        mSupplierNameEditText.setOnTouchListener(mTouchListener);
+        mSupplierPhoneEditText.setOnTouchListener(mTouchListener);
     }
 
     @Override
