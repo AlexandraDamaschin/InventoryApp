9a1294b80cca7839af576cd62221acdbf8487ba2
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-03 18:49:12 +0100

    ProductCursorAdapter implemented

--- app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
+++ app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
@@ -1,4 +1,57 @@
 package com.example.android.inventoryapp;
 
-public class ProductCursorAdapter {
+import android.content.Context;
+import android.database.Cursor;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CursorAdapter;
+import android.widget.TextView;
+
+import com.example.android.inventoryapp.data.InventoryContract;
+
+public class ProductCursorAdapter extends CursorAdapter {
+
+    //constructor
+    public ProductCursorAdapter(Context context, Cursor c) {
+        super(context, c, 0);
+    }
+
+    //Makes a new blank list item view. No data is set (or bound) to the views yet
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Inflate a list item view using the layout specified in list_item.xml
+        return LayoutInflater.from(context).inflate(R.layout.list_item, parent, false);
+    }
+
+    //This method binds the inventory data
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        // Find individual views that we want to modify in the list item layout
+        TextView nameTextView = (TextView) view.findViewById(R.id.name);
+        TextView priceTextView = (TextView) view.findViewById(R.id.price);
+        TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
+
+        // Find the columns of product attributes that we're interested in
+        int nameColumnIndex = cursor.getColumnIndex(InventoryContract.ProductEntry.COLUMN_PRODUCT_NAME);
+        int priceColumnIndex = cursor.getColumnIndex(InventoryContract.ProductEntry.COLUMN_PRODUCT_PRICE);
+        int quantityColumnIndex = cursor.getColumnIndex(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY);
+
+        // Read the product attributes from the Cursor for the current product
+        String productName = cursor.getString(nameColumnIndex);
+        String productPrice = cursor.getString(priceColumnIndex);
+        String productQuantity = cursor.getString(quantityColumnIndex);
+
+        // If the pet price is empty string or null, then use some default text
+        // that says "Unknown price", so the TextView isn't blank.
+        if (TextUtils.isEmpty(productPrice)) {
+            productPrice = context.getString(R.string.unknown_price);
+        }
+
+        // Update the TextViews with the attributes for the current product
+        nameTextView.setText(productName);
+        quantityTextView.setText(productQuantity);
+    }
+
 }
