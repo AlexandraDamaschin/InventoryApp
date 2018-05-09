a5eef7428c8629fe4d1fe4fbaa215c433e66027b
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-09 22:07:12 +0100

    sales button done

--- app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
+++ app/src/main/java/com/example/android/inventoryapp/ProductCursorAdapter.java
@@ -1,8 +1,8 @@
 package com.example.android.inventoryapp;
 
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.text.TextUtils;
@@ -17,9 +17,6 @@ import android.widget.Toast;
 import com.example.android.inventoryapp.data.InventoryContract;
 
 public class ProductCursorAdapter extends CursorAdapter {
-    // Content URI for the existing product (null if it's a new product)
-    private Uri mCurrentProductUri;
-
     //constructor
     public ProductCursorAdapter(Context context, Cursor c) {
         super(context, c, 0);
@@ -34,11 +31,14 @@ public class ProductCursorAdapter extends CursorAdapter {
 
     //This method binds the inventory data
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    public void bindView(View view, final Context context, final Cursor cursor) {
         // Find individual views that we want to modify in the list item layout
         TextView nameTextView = (TextView) view.findViewById(R.id.name);
         TextView priceTextView = (TextView) view.findViewById(R.id.price);
         TextView quantityTextView = (TextView) view.findViewById(R.id.quantity);
+        //call order button when is clicked to decrease quantity
+        Button quantityDecreased = (Button) view.findViewById(R.id.order);
+
 
         // Find the columns of product attributes that we're interested in
         int nameColumnIndex = cursor.getColumnIndex(InventoryContract.ProductEntry.COLUMN_PRODUCT_NAME);
@@ -48,7 +48,7 @@ public class ProductCursorAdapter extends CursorAdapter {
         // Read the product attributes from the Cursor for the current product
         String productName = cursor.getString(nameColumnIndex);
         String productPrice = cursor.getString(priceColumnIndex);
-        final int productQuantity = cursor.getInt(quantityColumnIndex);
+        String productQuantity = cursor.getString(quantityColumnIndex);
 
         // If the pet price is empty string or null, then use some default text
         // that says "Unknown price", so the TextView isn't blank.
@@ -61,20 +61,31 @@ public class ProductCursorAdapter extends CursorAdapter {
         quantityTextView.setText(productQuantity);
         priceTextView.setText(productPrice);
 
+        //get current quantity from database
+        final int currentQuantity = cursor.getInt(cursor.getColumnIndexOrThrow(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY));
+        //for the product clicked
+        String currentId = cursor.getString(cursor.getColumnIndexOrThrow(InventoryContract.ProductEntry._ID));
 
-        //call order button when is clicked to decrease quantity
-        Button quantityDecreased = (Button) view.findViewById(R.id.order);
+        final Uri currentUri = ContentUris.withAppendedId(InventoryContract.ProductEntry.CONTENT_URI, Long.parseLong(currentId));
 
         // Setup the item click listener
         quantityDecreased.setOnClickListener(new View.OnClickListener() {
+            //on click
             @Override
-            public void onItemClick(View v) {
-                //decrease quantity
-                productQuantity--;
-                // put in content value
-                ContentValues values = new ContentValues();
-                values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, productQuantity);
-                int rowsUpdated = getContentResolver().update(mCurrentProductUri, values, null, null);
+            public void onClick(View v) {
+                //check to see if quantity is more than 1
+                if (currentQuantity > 0) {
+                    //decrease quantity by one
+                    ContentValues values = new ContentValues();
+                    int decreasedQuantity = currentQuantity - 1;
+                    values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, decreasedQuantity);
+                    //update the current value
+                    int newUpdate = context.getContentResolver().update(currentUri, values, null, null);
+                    if (newUpdate == 0)
+                        Toast.makeText(context, R.string.error_in_updating, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context, R.string.quantity_bigger_then_one, Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
