5e6c5465fb5c7cdd5ac589d48b4c38b6a4963efe
Alexandra Maria Damaschin <damaschin.alexandra.maria@gmail.com>
2018-05-10 21:39:38 +0100

    increase decrease

--- app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
+++ app/src/main/java/com/example/android/inventoryapp/EditorActivity.java
@@ -465,28 +465,35 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         //get current quantity for current product
         String quantity = mQuantityEditText.getText().toString();
         int currentQuantity = Integer.parseInt(quantity);
-        //increase quantity need to be bigger than 1
-        if (!TextUtils.isEmpty(quantity) && currentQuantity >= 0) {
-            //crease current quantity by one
-            currentQuantity++;
-            ContentValues values = new ContentValues();
-            values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, currentQuantity);
+        //if quantity field is empty
+        if (TextUtils.isEmpty(quantity)) {
+            //increase quantity need to be bigger than 1
+            if (currentQuantity >= 0) {
+                //crease current quantity by one
+                currentQuantity++;
+                ContentValues values = new ContentValues();
+                values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, currentQuantity);
 
-            int rowsUpdated = getContentResolver().update(mCurrentProductUri, values, null, null);
+                int rowsUpdated = getContentResolver().update(mCurrentProductUri, values, null, null);
 
-            // Show a toast message depending on whether or not the increase was successful or not
-            //fail
-            if (rowsUpdated == -1) {
-                // If the row uri is null, then there was an error with insertion.
-                Toast.makeText(this, getString(R.string.increase_error), Toast.LENGTH_SHORT).show();
-            }
-            //success
-            else {
-                Toast.makeText(this, getString(R.string.increase_success), Toast.LENGTH_SHORT).show();
+                // Show a toast message depending on whether or not the increase was successful or not
+                //fail
+                if (rowsUpdated == -1) {
+                    // If the row uri is null, then there was an error with insertion.
+                    Toast.makeText(this, getString(R.string.increase_error), Toast.LENGTH_SHORT).show();
+                }
+                //success
+                else {
+                    Toast.makeText(this, getString(R.string.increase_success), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                //if quantity is less than 0 display toast message
+                Toast.makeText(this, getString(R.string.invalid_quantity), Toast.LENGTH_SHORT).show();
             }
         } else {
-            //if quantity is less than 0 display toast message
-            Toast.makeText(this, getString(R.string.invalid_quantity), Toast.LENGTH_SHORT).show();
+            finish();
+            Toast.makeText(this, getString(R.string.quantity_empty), Toast.LENGTH_SHORT).show();
+            
         }
     }
 
@@ -495,30 +502,36 @@ public class EditorActivity extends AppCompatActivity implements LoaderManager.L
         //get current quantity for current product
         String quantity = mQuantityEditText.getText().toString();
         int currentQuantity = Integer.parseInt(quantity);
-        //decrease quantity need to be bigger than 1
-        if (!TextUtils.isEmpty(quantity) && currentQuantity >= 0) {
-            //crease current quantity by one
-            currentQuantity--;
+        //if quantity field is empty
+        if (!TextUtils.isEmpty(quantity)) {
+            //decrease quantity need to be bigger than 1
+            if (currentQuantity >= 0) {
+                //crease current quantity by one
+                currentQuantity--;
 
-            ContentValues values = new ContentValues();
-            values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, currentQuantity);
+                ContentValues values = new ContentValues();
+                values.put(InventoryContract.ProductEntry.COLUMN_PRODUCT_QUANTITY, currentQuantity);
 
-            int rowsUpdated = getContentResolver().update(mCurrentProductUri, values, null, null);
+                int rowsUpdated = getContentResolver().update(mCurrentProductUri, values, null, null);
 
-            // Show a toast message depending on whether or not the decrease was successful or not
-            //fail
-            if (rowsUpdated == -1) {
-                // If the row uri is null, then there was an error with insertion.
-                Toast.makeText(this, getString(R.string.decrease_error), Toast.LENGTH_SHORT).show();
+                // Show a toast message depending on whether or not the decrease was successful or not
+                //fail
+                if (rowsUpdated == -1) {
+                    // If the row uri is null, then there was an error with insertion.
+                    Toast.makeText(this, getString(R.string.decrease_error), Toast.LENGTH_SHORT).show();
+                }
+                //success
+                else {
+                    Toast.makeText(this, getString(R.string.decrease_success), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                //if quantity is less than 0 display toast message
+                Toast.makeText(this, getString(R.string.invalid_quantity), Toast.LENGTH_SHORT).show();
             }
-            //success
-            else {
-                Toast.makeText(this, getString(R.string.decrease_success), Toast.LENGTH_SHORT).show();
-            }
+
         } else {
-            //if quantity is less than 0 display toast message
-            Toast.makeText(this, getString(R.string.invalid_quantity), Toast.LENGTH_SHORT).show();
+            finish();
+            Toast.makeText(this, getString(R.string.quantity_empty), Toast.LENGTH_SHORT).show();
         }
-
     }
 }
